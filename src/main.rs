ccf0667db2dadafca20b99067180240fe836c1ec
DotTejada <dotdudeperson@gmail.com>
2024-05-21 10:08:50 -0500

    On track to a way to codify each char

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,26 @@ fn make_node(key: char, value: i32) -> Node {
     Node { freq: (key, value), left: None, right: None }
 }
 
-fn find_codes(n: Node) {
+fn is_leaf(n: &Node) -> bool {
+    if n.left.is_none() && n.right.is_none() { return true } else { return false }
+}
+
+fn traverse(n: Node) -> Vec<(char, i32, String)> {
     let mut stack: Vec<Node> = vec![n];
+    let mut result = Vec::new();
     while stack.len() > 0 {
         let current = stack.pop().unwrap();
+        if is_leaf(&current) { 
+            result.push((current.freq.0, current.freq.1, "leaf".to_string())) 
+        } else {
+            result.push((current.freq.0, current.freq.1, "parent".to_string())) 
+        }
         println!("{:?}: {:?}", current.freq.0, current.freq.1);
 
         if current.right.is_some() { stack.push(*current.right.unwrap()) }
         if current.left.is_some() { stack.push(*current.left.unwrap()) }
     }
+    return result
 }
 
 impl Ord for Node {
@@ -50,7 +61,7 @@ fn main() {
 
     let mut freqs: HashMap<char, i32> = HashMap::new();
     let mut heap = BinaryHeap::new();
-    let input: String = String::from("avadakadavra");
+    let input: String = String::from("avadakadavrabbbbbb");
 
     for c in input.chars() {
         freqs.entry(c).and_modify(|c| *c += 1).or_insert(1);
@@ -72,6 +83,8 @@ fn main() {
         parent.right = Some(Box::new(right));
         heap.push(parent);
     }
-    println!("{:#?}", heap);
-    find_codes(heap.pop().unwrap())
+    //println!("{:#?}", heap);
+    let x = traverse(heap.pop().unwrap());
+    println!("{:?}", x);
+
 }
