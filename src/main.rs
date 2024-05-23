4392b28dd4b66fc02b80948a04dcb853ac92e24b
DotTejada <dotdudeperson@gmail.com>
2024-05-22 21:47:09 -0500

    working on it

--- src/main.rs
+++ src/main.rs
@@ -19,15 +19,16 @@ fn is_leaf(n: &Node) -> bool {
     if n.left.is_none() && n.right.is_none() { return true } else { return false }
 }
 
-fn traverse(n: Node) -> Vec<(char, i32, String)> {
+fn traverse(n: Node) -> Vec<(char, i32, &'static str)> {
     let mut stack: Vec<Node> = vec![n];
     let mut result = Vec::new();
     while stack.len() > 0 {
         let current = stack.pop().unwrap();
+
         if is_leaf(&current) { 
-            result.push((current.freq.0, current.freq.1, "leaf".to_string())) 
+            result.push((current.freq.0, current.freq.1, "leaf")) 
         } else {
-            result.push((current.freq.0, current.freq.1, "parent".to_string())) 
+            result.push((current.freq.0, current.freq.1, "parent")) 
         }
         println!("{:?}: {:?}", current.freq.0, current.freq.1);
 
@@ -61,7 +62,8 @@ fn main() {
 
     let mut freqs: HashMap<char, i32> = HashMap::new();
     let mut heap = BinaryHeap::new();
-    let input: String = String::from("avadakadavrabbbbbb");
+    //let input: String = String::from("avadakadavrabbbbbb");
+    let input: String = String::from("abcdbbaadaa");
 
     for c in input.chars() {
         freqs.entry(c).and_modify(|c| *c += 1).or_insert(1);
@@ -84,7 +86,35 @@ fn main() {
         heap.push(parent);
     }
     //println!("{:#?}", heap);
-    let x = traverse(heap.pop().unwrap());
-    println!("{:?}", x);
+    let tree = traverse(heap.pop().unwrap());
+    println!("{:?}", tree);
 
+    let mut i: usize = 1;
+    let mut lcount: u8 = 0;
+    let mut pcount: u8 = 0;
+    let mut code = String::new();
+    while i < tree.len() {
+        match tree[i].2 {
+            "parent" => { 
+                pcount += 1;
+                if pcount == 1 {
+                    code.push('0'); 
+                    i += 1; 
+                } else {
+                }
+                lcount = 0;
+            },
+            "leaf" => { 
+                lcount += 1;
+                if lcount == 1 {
+                    code.push('0'); 
+                } else {
+                    code.push('1'); 
+                }
+                println!("{:?}: {:?}", tree[i].0, code); 
+                code.pop(); 
+            },
+            _ => unreachable!()
+        }
+    }
 }
